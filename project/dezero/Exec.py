f2e0ce6295ff18a61a9585ee300383efea3d9231
CycloneFox19 <satoru21131@gmail.com>
2024-02-24 19:28:19 +0900

    by step 22

--- project/dezero/Exec.py
+++ project/dezero/Exec.py
@@ -3,5 +3,16 @@ sys.path.append(os.pardir)
 from steps.step21 import *
 
 x = Variable(np.array(2.0))
-y = 3.0 * x + 1.0
-print(y)
+y1 = 3.0 / x
+y2 = x / 1.5
+y3 = x ** 3
+
+print(y1)
+print(y2)
+print(y3)
+
+y3.backward(True)
+
+print(y3.grad)
+print(y2.grad)
+print(y1.grad)
